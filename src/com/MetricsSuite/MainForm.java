f8e767c06ddea4e1c25b8a72f72e41508fa79798
sharmaarjun360 <sharma.arjun360@gmail.com>
2020-02-07 13:53:01 -0800

    second commit

---
+++ src/com/MetricsSuite/MainForm.java
@@ -0,0 +1,20 @@
+package com.MetricsSuite;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainForm {
+
+    private JPanel mainPanel;
+
+    public static void main(String[] args) {
+        JFrame mainFrame = new JFrame("MetricSuit");
+        mainFrame.setContentPane(new MainForm().mainPanel);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        mainFrame.setSize(200,200);
+        mainFrame.setLocation((screenSize.width)/2,(screenSize.height)/2);
+//        mainFrame.pack(); packs according to components size
+        mainFrame.setVisible(true);
+    }
+}
