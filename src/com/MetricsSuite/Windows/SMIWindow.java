aee38a0fd1b9014f1698a095ae1d7befe70ed71e
Vivek <vivek1mishra@outlook.com>
2020-03-11 15:45:47 -0700

    Action listner added to add_row_btn

--- src/com/MetricsSuite/Windows/SMIWindow.java
+++ src/com/MetricsSuite/Windows/SMIWindow.java
@@ -1,17 +1,16 @@
 package com.MetricsSuite.Windows;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SMIWindow  extends JFrame{
+public class SMIWindow {
     MainWindow mainWindow;
-    public String[] columnNames = {"SMI",
-            "Module Added",
-            "Modules Changed",
-            "Modules Deleted",
-            "Total Module"};
-    public Object[][] data = {
-    };
+    public JButton add_row_btn, compute_index_btn;
+    public JTable smiTable;
+    public DefaultTableModel model;
 
     public JComponent createNewSMIPanel(){
         JPanel panel = new JPanel(false);
@@ -19,12 +18,18 @@ public class SMIWindow  extends JFrame{
         JLabel SMI_heading = new JLabel("Software Maturity Index");
         SMI_heading.setFont((new Font("", Font.BOLD, 15)));
 
-        JTable smiTable = new JTable(data,columnNames);
+        model= new DefaultTableModel();
+        smiTable = new JTable(model);
+        model.addColumn("SMI");
+        model.addColumn("Modules Added");
+        model.addColumn("Modules Changed");
+        model.addColumn("Modules Deleted");
+        model.addColumn("Total Module");
         JScrollPane scrollPane = new JScrollPane(smiTable);
         smiTable.setFillsViewportHeight(true);
 
-        JButton add_row_btn = new JButton("Add Row");
-        JButton compute_index_btn = new JButton("Compute Index");
+        add_row_btn = new JButton("Add Row");
+        compute_index_btn = new JButton("Compute Index");
 
         SMI_heading.setBounds(200,5,200,20);
         scrollPane.setBounds(20,50,540,360);
@@ -36,8 +41,17 @@ public class SMIWindow  extends JFrame{
         panel.add(compute_index_btn);
         panel.add(add_row_btn);
 
+        add_row_btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                model.addRow(new Object[]{ " ", " ", " ", " ", " " });
+            }
+        });
+
         panel.setLayout(null);
         return panel;
+
+
 
     }
 
