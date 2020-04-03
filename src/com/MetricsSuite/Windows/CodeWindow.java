c601bbe01f3c243082b5b11446c9e1b1a3f6a982
Vivek <vivek1mishra@outlook.com>
2020-04-03 01:18:01 -0700

    Added Project data pane

---
+++ src/com/MetricsSuite/Windows/CodeWindow.java
@@ -0,0 +1,13 @@
+package com.MetricsSuite.Windows;
+
+import javax.swing.*;
+
+public class CodeWindow extends JFrame {
+
+    public JComponent createNewDataPanel ()
+    {
+        JPanel panel = new JPanel(false);
+        panel.setLayout(null);
+        return panel;
+    }
+}
