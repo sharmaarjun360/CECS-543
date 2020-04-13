c88c1e17d5b8b5d4a4369fb03bf2d19f58b129d9
Vivek <vivek1mishra@outlook.com>
2020-04-13 14:19:13 -0700

    Added scroll bar to the CodeWindow

--- src/com/MetricsSuite/Windows/CodeWindow.java
+++ src/com/MetricsSuite/Windows/CodeWindow.java
@@ -1,12 +1,10 @@
 package com.MetricsSuite.Windows;
 
-import com.MetricsSuite.GlobalConstants.MetricsConstants;
 import com.MetricsSuite.Models.ProjectStatistics;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.tree.TreePath;
-import java.awt.*;
 import java.io.File;
 
 public class CodeWindow extends JFrame {
@@ -29,18 +27,23 @@ public class CodeWindow extends JFrame {
         statistics.calculateStatistics();
 
         JPanel panel = new JPanel(false);
+        JPanel secondaryPanel = new JPanel(false);
+        secondaryPanel.setLayout(new BoxLayout(secondaryPanel,BoxLayout.Y_AXIS));
+        //secondaryPanel.setBorder(new EmptyBorder(21,11,1,1));
         panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
-        panel.setBorder(new EmptyBorder(20,10,0,0));
+       // panel.setBorder(new EmptyBorder(20,10,0,0));
+
+
 
         JLabel fileMeta = new JLabel("<html>File name: "+this.selectedFile.getName() +
                 "<br>File length in bytes: "+statistics.getFileSize() +
                 "<br>File white space: "+statistics.getWs() +
                 "<br>File comment space in bytes: "+ statistics.getCommentSpace() +
                 "<br>Comment percentage of file: "+ statistics.getCommentPercentage()+"%</html>");
-        panel.add(fileMeta);
+        secondaryPanel.add(fileMeta);
 
         JLabel label = new JLabel("Halstead metrics: ");
-        panel.add(label);
+        secondaryPanel.add(label);
 
         JLabel halsteadMetricsValue = new JLabel("<html>" +
                 "Unique operators: "+statistics.getUniqueOperators() +
@@ -56,14 +59,16 @@ public class CodeWindow extends JFrame {
                 "</html>"
         );
         halsteadMetricsValue.setBorder(new EmptyBorder(0,10,30,0));
-        panel.add(halsteadMetricsValue);
+        secondaryPanel.add(halsteadMetricsValue);
 
         label = new JLabel("McCabe's Cyclomatic Complexity:");
-        panel.add(label);
+        secondaryPanel.add(label);
 
         JLabel mcCabeValue = new JLabel("<html>" + statistics.getMccabeValues() + "</html>");
         mcCabeValue.setBorder(new EmptyBorder(0,10,0,0));
-        panel.add(mcCabeValue);
+        secondaryPanel.add(mcCabeValue);
+        JScrollPane scrollPane = new JScrollPane(secondaryPanel);
+        panel.add(scrollPane);
 
         return panel;
     }
